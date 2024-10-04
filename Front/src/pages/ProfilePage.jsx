f41af12cac4c1b88074c86669ef812220cc18016
Lilianannette <lilianannettepro@gmail.com>
2024-10-04 23:30:29 +0200

    update json file

--- Front/src/pages/ProfilePage.jsx
+++ Front/src/pages/ProfilePage.jsx
@@ -13,7 +13,31 @@ export const ProfilePage = () => {
     }
     try {
       const UserProfile = await GetProfile(token);
-      // completer la Page Getprofile
+        setUser(UserProfile);
+    } catch (err) {
+      setError('Erreur lors de la récupération du profil.', err);
     }
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+  if (error) {
+    return <div style={{ color: 'red'}}>{error}</div>;
   }
+
+  if(!user) {
+    return <div>Chargement des informations...</div>;
+  }
+
+  return (
+    <div>
+        <h1>Mon Profil</h1>
+        <p><strong>Nom :</strong> {user.lastname}</p>
+        <p><strong>Prénom :</strong> {user.firstname}</p>
+        <p><strong>Pseudo :</strong> {user.pseudo}</p>
+        <p><strong>Email :</strong> {user.email}</p>
+        <p><strong>Genre :</strong> {user.gender}</p>
+    </div>
+  );
 }
