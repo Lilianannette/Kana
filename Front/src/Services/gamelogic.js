cb32e71adcb9153c12b51c084b8e5e4730276e9d
lilian annette <lilianannettepro@gmail.com>
2024-10-16 01:45:32 +0200

    fix lvl 1 to 3

--- Front/src/Services/gamelogic.js
+++ Front/src/Services/gamelogic.js
@@ -1,109 +1,166 @@
 const generateOptions = (correctKana, kanaList, answerKey) => {
+  if (!correctKana || !kanaList || !correctKana[answerKey] || kanaList.length === 0) {
+      console.error('Kana incorrect ou Liste de kana vide.', correctKana, kanaList);
+      return [];
+  }
+
   let options = [correctKana[answerKey]];
   while (options.length < 4) {
-    const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
-    if(!options.includes(randomKana[answerKey])) {
-      options.push(randomKana[answerKey]);
-    }
+      const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
+      if (randomKana && randomKana[answerKey] && !options.includes(randomKana[answerKey])) {
+          options.push(randomKana[answerKey]);
+      }
   }
   return options.sort(() => Math.random() - 0.5);
 };
 
-export const level1logic = (kanaList, currentScore, setScore, setLevel) => {
-  if(!kanaList || kanaList.length === 0) {
+export const level1logic = (currentKana, currentScore, setScore, setLevel, kanaList, setCurrentKana, lastKana, setLastKana) => {
+  if (!currentKana || !kanaList || kanaList.length === 0 || !currentKana.romanization) {
+    console.error("Kana ou Liste de kana incorrecte.", currentKana, kanaList);
     return {
-      question: null,
+      question: '',
       options: [],
       handleAnswer: () => {},
     };
   }
 
-  const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
-  const options = generateOptions(randomKana, kanaList, 'romanization');
+  const options = generateOptions(currentKana, kanaList, 'romanization');
 
   const handleAnswer = (answer) => {
-    if (answer === randomKana.romanization) {
+    if (answer === currentKana.romanization) {
       setScore((prevScore) => prevScore + 3);
     } else {
       setScore((prevScore) => prevScore - 1);
     }
+
+    let newKana;
+    do {
+      newKana = kanaList[Math.floor(Math.random() * kanaList.length)];
+    } while (newKana === lastKana);
+
+    setCurrentKana(newKana);
+    setLastKana(newKana);
+
     if (currentScore + 3 >= 20) {
+      setScore(0);
       setLevel(2);
     }
   };
 
   return {
-    question: randomKana.character,
+    question: currentKana.character,
     options: options,
     handleAnswer: handleAnswer,
-  }
+  };
 };
 
-export const level2logic = (kanaList, currentScore, setScore, setLevel) => {
-  const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
-  const options = generateOptions(randomKana, kanaList, 'character');
+export const level2logic = (currentKana, currentScore, setScore, setLevel, setCurrentKana, kanaList, lastKana, setLastKana) => {
+  if (!currentKana || !kanaList || kanaList.length === 0 || !currentKana.character) {
+    console.error("Kana ou Liste de kana incorrecte.", currentKana, kanaList);
+    return {
+      question: '',
+      options: [],
+      handleAnswer: () => {},
+    };
+  }
+
+  const options = generateOptions(currentKana, kanaList, 'character');
 
   const handleAnswer = (answer) => {
-    if (answer === randomKana.character) {
+    if (answer === currentKana.character) {
       setScore((prevScore) => prevScore + 3);
     } else {
       setScore((prevScore) => prevScore - 1);
     }
 
-    if(currentScore + 3 >= 20) {
+    let newKana;
+    do {
+      newKana = kanaList[Math.floor(Math.random() * kanaList.length)];
+    } while (newKana === lastKana);
+
+    setCurrentKana(newKana);
+    setLastKana(newKana);
+
+    if (currentScore + 3 >= 20) {
+      setScore(0);
       setLevel(3);
     }
   };
 
   return {
-    question: randomKana.romanization,
+    question: currentKana.romanization,
     options: options,
     handleAnswer: handleAnswer,
   };
 };
 
-export const level3logic = (kanaList, currentScore, setScore, setLevel) => {
-  const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
+export const level3logic = (currentKana, currentScore, setScore, setLevel, kanaList, lastKana, setCurrentKana, setLastKana) => {
+  if (!currentKana || !currentKana.character || !currentKana.romanization) {
+    console.log('Kana incorrect:', currentKana);
+    return {
+      question: '',
+      handleAnswer: () => {},
+    };
+  }
 
   const handleAnswer = (userInput) => {
-    if (userInput.toLowerCase() === randomKana.romanization.toLowerCase()) {
+    if (userInput.toLowerCase() === currentKana.romanization.toLowerCase()) {
       setScore((prevScore) => prevScore + 3);
     } else {
       setScore((prevScore) => prevScore - 1);
     }
+
+    let newKana;
+    do {
+      newKana = kanaList[Math.floor(Math.random() * kanaList.length)];
+    } while (newKana === lastKana);
+
+    setCurrentKana(newKana);
+    setLastKana(newKana);
+
     if (currentScore + 3 >= 20) {
+      setScore(0);
       setLevel(4);
     }
   };
 
   return {
-    question: randomKana.character,
+    question: currentKana.character,
     handleAnswer: handleAnswer,
   };
 };
 
 export const level4logic = (kanaList, currentScore, setScore) => {
+  if (!kanaList || kanaList.length === 0) {
+      console.error("Liste de kana vide ou incorrecte.");
+      return {
+          question: '',
+          handleAnswer: () => {},
+      };
+  }
+
   const randomKanaSequence = [];
   for (let i = 0; i < 3; i++) {
-    const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
-    randomKanaSequence.push(randomKana);
+      const randomKana = kanaList[Math.floor(Math.random() * kanaList.length)];
+      randomKanaSequence.push(randomKana);
   }
 
   const correctAnswer = randomKanaSequence.map(kana => kana.romanization).join('');
+  console.log('Séquence aléatoire pour le niveau 4:', randomKanaSequence);
 
   const handleAnswer = (userInput) => {
-    if (userInput.toLowerCase() === correctAnswer.toLowerCase()) {
-      setScore((prevScore) => prevScore + 3);
-    } else {
-      setScore((prevScore) => prevScore - 1);
-    }
-    if (currentScore + 3 >= 20) {
-      return 'Partie terminé'
-    }
+      if (userInput.toLowerCase() === correctAnswer.toLowerCase()) {
+          setScore((prevScore) => prevScore + 3);
+      } else {
+          setScore((prevScore) => prevScore - 1);
+      }
+      if (currentScore + 3 >= 20) {
+          return 'Partie terminée';
+      }
   };
 
   return {
-    question: randomKanaSequence.map(kana => kana.character).join(''),
-    handleAnswer: handleAnswer,
+      question: randomKanaSequence.map(kana => kana.character).join(''),
+      handleAnswer: handleAnswer,
   };
 };
--- Front/src/pages/GamePage.jsx
+++ Front/src/pages/GamePage.jsx
@@ -1,108 +1,13 @@
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import hiraganaList from '../assets/data/hiragana.json';
-// import katakanaList from '../assets/data/katakana.json';
-// import axios from 'axios';
-
-// export const GamePage = () => {
-//     const navigate = useNavigate();
-
-//     const [selectedLines, setSelectedLines] = useState({
-//         hiragana: [],
-//         katakana: [],
-//     });
-
-//     const [isGameReady, setIsGameReady] = useState(false);
-
-//     const handleLineSelection = (line, type) => {
-//         const updatedSelection = selectedLines[type].includes(line)
-//             ? selectedLines[type].filter(selectedLine => selectedLine !== line)
-//             : [...selectedLines[type], line];
-
-//         setSelectedLines({
-//             ...selectedLines,
-//             [type]: updatedSelection,
-//         });
-
-//         setIsGameReady(updatedSelection.length > 0 || selectedLines.hiragana.length > 0);
-//     };
-
-//     const handleStartGame = async () => {
-//         const selectedKana = [
-//             ...selectedLines.hiragana.flat(),
-//             ...selectedLines.katakana.flat(),
-//         ];
-
-//         try {
-//           const response = await axios.post('http://localhost:3000/api/game', {
-//             kanaList: selectedKana,
-//             level: 1,
-//           }, {
-//             headers: {
-//               Authorization: `Bearer ${localStorage.getItem('token')}`
-//             }
-//           });
-//           if(response.status === 201) {
-//             navigate('/start', { state: { selectedKana } });
-//           }
-//         } catch (err) {
-//           console.error('Erreur lors de la création de la partie:', err);
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <h1>Choisissez les lignes à pratiquer</h1>
-
-//             <h2>Hiragana</h2>
-//             {hiraganaList.map((line, idx) => (
-//                 <div key={idx} style={{ marginBottom: '10px' }}>
-//                     <input
-//                         type="checkbox"
-//                         onChange={() => handleLineSelection(line, 'hiragana')}
-//                         checked={selectedLines.hiragana.includes(line)}
-//                     />
-//                     {line.map((kana, i) => (
-//                         <span key={kana.char_id} style={{ marginRight: '10px' }}>
-//                             {kana.romanization} {i < line.length - 1 && ' • '}
-//                         </span>
-//                     ))}
-//                 </div>
-//             ))}
-
-//             <h2>Katakana</h2>
-//             {katakanaList.map((line, idx) => (
-//                 <div key={idx} style={{ marginBottom: '10px' }}>
-//                     <input
-//                         type="checkbox"
-//                         onChange={() => handleLineSelection(line, 'katakana')}
-//                         checked={selectedLines.katakana.includes(line)}
-//                     />
-//                     {line.map((kana, i) => (
-//                         <span key={kana.char_id} style={{ marginRight: '10px' }}>
-//                             {kana.romanization} {i < line.length - 1 && ' • '}
-//                         </span>
-//                     ))}
-//                 </div>
-//             ))}
-
-//             <button
-//                 onClick={handleStartGame}
-//                 disabled={!isGameReady}
-//             >
-//                 Commencer le jeu
-//             </button>
-//         </div>
-//     );
-// };
-
-
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import hiraganaList from '../assets/data/hiragana.json';
 import katakanaList from '../assets/data/katakana.json';
 import { level1logic, level2logic, level3logic, level4logic } from '../Services/gameLogic';
 import axios from 'axios';
+
+const shuffleArray = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+};
 
 export const GamePage = () => {
     const navigate = useNavigate();
@@ -114,11 +19,13 @@ export const GamePage = () => {
 
     const [currentLevel, setCurrentLevel] = useState(1);
     const [currentScore, setCurrentScore] = useState(0);
+    const [currentKanaList, setCurrentKanaList] = useState([]);
     const [currentKana, setCurrentKana] = useState(null);
     const [options, setOptions] = useState([]);
     const [userInput, setUserInput] = useState('');
     const [handleAnswer, setHandleAnswer] = useState(null);
     const [isGameReady, setIsGameReady] = useState(false);
+    const [lastKana, setLastKana] = useState(null);
 
     const handleLineSelection = (line, type) => {
         const updatedSelection = selectedLines[type].includes(line)
@@ -134,77 +41,75 @@ export const GamePage = () => {
     };
 
     const handleStartGame = async () => {
-        const selectedKana = [
+        const selectedKanaLines = [
             ...selectedLines.hiragana.flat(),
             ...selectedLines.katakana.flat(),
         ];
 
-        try {
-          const token = localStorage.getItem('token');
-
-          if (!token) {
-            console.error('Token manquant. Veuillez vous connecter.');
+        if (selectedKanaLines.length === 0) {
+            console.error('Aucune ligne sélectionnée');
             return;
-          }
-
-          const response = await axios.post('http://localhost:3000/api/game', {
-            kanaList: selectedKana,
-            level:1,
-          }, {
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          });
-
-          if (response === 201) {
-            setCurrentKana(selectedKana);
-            setCurrentLevel(1);
-          }
-        } catch (err) {
-          console.error('Erreur lors de la création de la partie:', err);
         }
 
-        // try {
-        //   const response = await axios.post('http://localhost:3000/api/game', {
-        //     kanaList: selectedKana,
-        //     level: 1,
-        //   }, {
-        //     headers: {
-        //       Authorization: `Bearer ${localStorage.getItem('token')}`
-        //     }
-        //   });
-        //   if (response.status === 201) {
-        //     setCurrentKana(selectedKana);
-        //     setCurrentLevel(1); // Démarre au niveau 1
-        //   }
-        // } catch (err) {
-        //   console.error('Erreur lors de la création de la partie:', err);
-        // }
+        try {
+            const token = localStorage.getItem('token');
+
+            if (!token) {
+                console.error('Token manquant. Veuillez vous connecter.');
+                return;
+            }
+
+            const response = await axios.post('http://localhost:3000/api/game', {
+                kanaList: selectedKanaLines,
+                level: 1,
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            if (response.status === 201) {
+                const shuffledKanaList = shuffleArray(selectedKanaLines);
+                console.log("Liste des kana mélangés :", shuffledKanaList);
+
+                setCurrentKanaList(shuffledKanaList);
+                setCurrentKana(shuffledKanaList[0]);
+                setCurrentLevel(1);
+            }
+        } catch (err) {
+            console.error('Erreur lors de la création de la partie, Veuillez vous connecter', err);
+        }
     };
 
     useEffect(() => {
-        if (currentLevel === 1) {
-            const { question, options, handleAnswer } = level1logic(currentKana, currentScore, setCurrentScore, setCurrentLevel);
-            setCurrentKana(question);
-            setOptions(options);
-            setHandleAnswer(() => handleAnswer);
-        } else if (currentLevel === 2) {
-            const { question, options, handleAnswer } = level2logic(currentKana, currentScore, setCurrentScore, setCurrentLevel);
-            setCurrentKana(question);
-            setOptions(options);
-            setHandleAnswer(() => handleAnswer);
-        } else if (currentLevel === 3) {
-            const { question, handleAnswer } = level3logic(currentKana, currentScore, setCurrentScore, setCurrentLevel);
-            setCurrentKana(question);
-            setOptions([]);
-            setHandleAnswer(() => handleAnswer);
-        } else if (currentLevel === 4) {
-            const { question, handleAnswer } = level4logic(currentKana, currentScore, setCurrentScore);
-            setCurrentKana(question);
-            setOptions([]);
-            setHandleAnswer(() => handleAnswer);
+      if (currentKana && currentLevel === 1) {
+        const { options, handleAnswer } = level1logic(currentKana, currentScore, setCurrentScore, setCurrentLevel, currentKanaList, setCurrentKana, lastKana, setLastKana);
+        setOptions(options);
+        setHandleAnswer(() => handleAnswer);
+      } else if (currentKana && currentLevel === 2) {
+        const { options, handleAnswer } = level2logic(currentKana, currentScore, setCurrentScore, setCurrentLevel, setCurrentKana, currentKanaList, lastKana, setLastKana);
+        setOptions(options);
+        setHandleAnswer(() => handleAnswer);
+      } else if (currentKana && currentLevel === 3) {
+        const { handleAnswer } = level3logic(currentKana, currentScore, setCurrentScore, setCurrentLevel, currentKanaList, lastKana, setCurrentKana, setLastKana);
+        setOptions([]);
+        setHandleAnswer(() => handleAnswer);
+      } else if (currentKana && currentLevel === 4) {
+        const { handleAnswer } = level4logic(currentKanaList, currentScore, setCurrentScore, lastKana);
+        setOptions([]);
+        setHandleAnswer(() => handleAnswer);
+      }
+    }, [currentLevel, currentKana, currentScore, currentKanaList, lastKana]);
+
+    const handleNextKana = () => {
+        const nextIndex = currentKanaList.indexOf(currentKana) + 1;
+
+        if (nextIndex < currentKanaList.length) {
+            setCurrentKana(currentKanaList[nextIndex]);
+        } else {
+            console.log("Tous les kana ont été utilisés.");
         }
-    }, [currentLevel, currentKana, currentScore]);
+    };
 
     const handleSubmit = () => {
         if (handleAnswer) {
@@ -213,10 +118,12 @@ export const GamePage = () => {
             } else if (currentLevel === 3 || currentLevel === 4) {
                 const result = handleAnswer(userInput);
 
-                if (result === 'Partie terminé') {
+                if (result === 'Partie terminée') {
                     navigate('/');
                 }
             }
+
+            handleNextKana();
         }
         setUserInput('');
     };
@@ -224,7 +131,6 @@ export const GamePage = () => {
     return (
         <div>
             <h1>Niveau {currentLevel}</h1>
-            <h2>Score: {currentScore}</h2>
 
             <h2>Hiragana</h2>
             {hiraganaList.map((line, idx) => (
@@ -264,9 +170,10 @@ export const GamePage = () => {
                 </button>
             ) : null}
 
+            <h2>Score: {currentScore}</h2>
             {currentKana && (
                 <div>
-                    <h3>{currentKana}</h3> 
+                    <h3>{currentLevel === 2 ? currentKana.romanization : currentKana.character}</h3>
                     {currentLevel === 3 || currentLevel === 4 ? (
                         <div>
                             <input
@@ -291,3 +198,6 @@ export const GamePage = () => {
         </div>
     );
 };
+
+
+//PROBLEME AU LVL 3 - 4 TOUS LES SYMBOLE UTILISER CONSOLE LOG L110
